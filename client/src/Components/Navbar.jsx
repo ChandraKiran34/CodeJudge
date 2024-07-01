dcd0ee5877ee83895dfdd3dd7b119b871ebeadf4
ChandraKiran34 <bchakri06@gmail.com>
2024-07-01 22:42:49 +0530

    register frontend

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LuCode2 } from "react-icons/lu";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex justify-between ">
-        <div>
-          <h1 className="font-bold text-white mt-[0.6rem]">CODEJUDGE</h1>
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo and Brand */}
+        <div className="flex items-center">
+          <Link to="/" className="flex items-center text-white">
+            <LuCode2 className="text-white text-3xl mr-2" />
+            <h1 className="font-bold text-white text-2xl">Code Judge</h1>
+          </Link>
         </div>
-        <div className="text-white">
-          <div className="text-white">
-            <Link to="/" className="mr-4">
-              Home
-            </Link>
-            <Link to="/problems" className="mr-4">
-              Problems
-            </Link>
-            <Link to="/bookmarks" className="mr-4">
-              Bookmarks
-            </Link>
-            <Link to="/profile" className="mr-4">
-              Profile
-            </Link>
-            <Link to="/register" className="mr-4">
-              Register
-            </Link>
-            <Link to ='/login' >
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Login
-              </button>
-            </Link>
-          </div>
+
+        {/* Navigation Links */}
+        <div className="flex justify-center mt-4 md:mt-0">
+          <Link to="/" className="mr-4 text-white hover:text-gray-300">
+            Home
+          </Link>
+          <Link to="/problems" className="mr-4 text-white hover:text-gray-300">
+            Problems
+          </Link>
+          <Link to="/bookmarks" className="mr-4 text-white hover:text-gray-300">
+            Bookmarks
+          </Link>
+          <Link to="/profile" className="mr-4 text-white hover:text-gray-300">
+            Profile
+          </Link>
+          <Link to="/register" className="mr-4 text-white hover:text-gray-300">
+            Register
+          </Link>
+        </div>
+
+        {/* Login Button */}
+        <div className="flex items-center">
+          <Link to="/login">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Login
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { MdPerson } from "react-icons/md";
 const RegisterPage = () => {
   const [inputData, setInputData] = useState({
     userName: "",
@@ -9,18 +9,38 @@ const RegisterPage = () => {
     github: "",
     linkedin: "",
   });
+  const [profilePicture, setProfilePicture] = useState(null);
   const [message, setMessage] = useState("");
+  const [file, setFile] = useState(null);
+  const formData = new FormData();
 
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    const file = event.target.files[0];
+    if (file) {
+      setProfilePicture(URL.createObjectURL(file));
+    }
+  };
+
+  
+  
+  
+  
   const handleSubmit = async (e) => {
-    console.log(inputData);
     e.preventDefault();
+    console.log(inputData);
+    formData.append("userName", inputData.userName);
+    formData.append("email", inputData.email);
+    formData.append("password", inputData.password);
+    formData.append("phone", inputData.phone);
+    formData.append("github", inputData.github);
+    formData.append("linkedin", inputData.linkedin);
+    formData.append('avatar', file);
     try {
       const response = await fetch("http://localhost:8000/auth/register", {
         method: "POST",
-        headers: {
-          "content-Type": "application/json",
-        },
-        body: JSON.stringify(inputData),
+        body: formData,
       });
       const data = await response.json();
       setMessage(data.message);
@@ -33,9 +53,10 @@ const RegisterPage = () => {
         github: "",
         linkedin: "",
       });
+      setProfilePicture(null);
     } catch (error) {
       console.log(error);
-      setMessage("registration not successful");
+      setMessage("Registration not successful");
     }
   };
 
@@ -48,129 +69,102 @@ const RegisterPage = () => {
   };
 
   const customStyles = {
-    // backgroundImage : `linear-gradient(to right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2024/01/Coding-Bootcamp-Statistics.jpg')`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
 
   return (
-    <div
-      className="min-h-[91vh] flex items-center justify-center bg-gray-100 px-4 py-5 sm:px-6 lg:px-8"
-      style={customStyles}
-    >
-      <div className="max-w-4xl w-full space-y-8">
-        <div>
-          <h1 className="mt-6 text-center text-3xl font-bold mb-[4rem]  text-gray-900">
-            User Registration
-          </h1>
-        </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-            <div className="col-span-1">
-              <label
-                htmlFor="userName"
-                className="block text-sm font-bold text-black"
-              >
-                Full Name
-              </label>
-              <input
-                type="text"
-                name="userName"
-                value={inputData.userName}
-                onChange={handleInputChange}
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="col-span-1">
-              <label
-                htmlFor="email"
-                className="block text-sm font-bold text-black"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                value={inputData.email}
-                onChange={handleInputChange}
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="col-span-1">
-              <label
-                htmlFor="password"
-                className="block text-sm font-bold text-black"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                value={inputData.password}
-                onChange={handleInputChange}
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="col-span-1">
-              <label
-                htmlFor="phone"
-                className="block text-sm font-bold text-black"
-              >
-                Phone Number
-              </label>
-              <input
-                type="tel"
-                name="phone"
-                value={inputData.phone}
-                onChange={handleInputChange}
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="col-span-1">
-              <label
-                htmlFor="github"
-                className="block text-sm font-bold text-black"
-              >
-                Github Link
-              </label>
-              <input
-                type="url"
-                name="github"
-                value={inputData.github}
-                onChange={handleInputChange}
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="col-span-1">
-              <label
-                htmlFor="linkedin"
-                className="block text-sm font-bold text-black"
-              >
-                Linkedin Profile
-              </label>
-              <input
-                type="url"
-                name="linkedin"
-                value={inputData.linkedin}
-                onChange={handleInputChange}
-                required
-                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="group relative w-[30%] ml-[18rem] mt-[5rem] flex justify-center py-2 px-4 border border-transparent text-sm font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    <div className="flex flex-col items-center justify-center h-screen dark  bg-pink-300">
+      <h2 className="text-4xl text-[#444] font-bold text- mb-5 flex items-center justify-center">
+        <MdPerson className="mr-[0.3rem] text-4xl" /> Register
+      </h2>
+      <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-md p-8">
+        {/* <label className="block text-gray-200 ">Profile Picture</label> */}
+        <form className="flex flex-col" onSubmit={handleSubmit} encType='multipart/form-data'>
+          <div className="flex items-center mb-4">
+            <label
+              htmlFor="profilePicture"
+              className="cursor-pointer flex items-center justify-center w-32 h-32 ml-[8.5rem] bg-gray-700 text-gray-200 border-0 rounded-full overflow-hidden focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150 "
             >
-              Submit
-            </button>
+              {profilePicture ? (
+                <img
+                  src={profilePicture}
+                  alt="Profile Preview"
+                  className="object-cover w-full h-full"
+                />
+              ) : (
+                <span className="text-sm">Upload Picture</span>
+              )}
+            </label>
+            <input
+              id="profilePicture"
+              type="file"
+              name="avatar"
+              onChange={handleFileChange}
+              className="hidden"
+            />
           </div>
+
+          <input
+            type="text"
+            name="userName"
+            value={inputData.userName}
+            onChange={handleInputChange}
+            required
+            placeholder="Full Name"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+          />
+          <input
+            placeholder="Email"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="email"
+            name="email"
+            value={inputData.email}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            placeholder="Password"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="password"
+            name="password"
+            value={inputData.password}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            placeholder="Phone"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="tel"
+            name="phone"
+            value={inputData.phone}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            placeholder="Github Link"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="url"
+            name="github"
+            value={inputData.github}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            placeholder="LinkedIn Profile"
+            className="bg-gray-700 text-gray-200 border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+            type="url"
+            name="linkedin"
+            value={inputData.linkedin}
+            onChange={handleInputChange}
+            required
+          />
+          <button
+            className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
+            type="submit"
+          >
+            Register
+          </button>
         </form>
       </div>
     </div>
